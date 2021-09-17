9138171b0b967c5774a5fddb1fe788a7d547d159
maxidefilippis <maxidefilippis@gmail.com>
2021-09-17 18:51:08 -0300

    [ADD] Pizzaiolo brand

--- src/layout/index.js
+++ src/layout/index.js
@@ -6,9 +6,12 @@ import "./index.scss";
 export default function Layout({ children }) {
   return (
     <div className="layout">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
-        <Link to="/">Inicio</Link>
-        <Link to="/add">Agregar</Link>
+      <nav className="layout__navbar navbar navbar-expand-lg navbar-light bg-light px-5">
+        <h1>PIZZAIOLO STOCK</h1>
+        <div className="layout__links">
+          <Link to="/">Inicio</Link>
+          <Link to="/add">Agregar</Link>
+        </div>
       </nav>
       <div>{children}</div>
     </div>
